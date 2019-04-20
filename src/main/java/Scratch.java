7d4e91081492a56303b07c55422f6bcc1a429471
Jarris <Hu.Jia.Jarris@Vertivco.com>
2019-04-20 19:00:52 +0800

    Init Commit

---
+++ src/main/java/Scratch.java
@@ -0,0 +1,33 @@
+import org.apache.commons.net.telnet.TelnetClient;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author Jarris
+ */
+public class Scratch {
+
+    public static void main(String[] args){
+        List<String> ips = new ArrayList<>();
+        for (int i = 2; i <= 254; i++) {
+            String ip = "10.169.42."+ i;
+            if(telnet(ip)){
+                ips.add(ip);
+            }
+        }
+        System.out.println(ips);
+    }
+
+    private static boolean telnet(String ipString){
+        TelnetClient telnetClient = new TelnetClient();
+        telnetClient.setConnectTimeout(100);
+
+        try {
+            telnetClient.connect(ipString, 8080);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+}
